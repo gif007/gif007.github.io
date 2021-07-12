223f46c6f8f8dd3afa86624be8d19d9c1e5009a9
bard <carruthers.n.t@gmail.com>
2021-07-12 11:27:53 -0300

    finished rough draft of super deals.  normal deals are styled with backgrounds but no hover effect

--- src/components/cruiseline-super-deals/cruiseline-super-deals.component.js
+++ src/components/cruiseline-super-deals/cruiseline-super-deals.component.js
@@ -1,18 +1,44 @@
 import React from 'react';
 
 import {
-    SuperDealsWrapper
+    SuperDealsWrapper,
+    Row,
+    Deal,
+    Title,
+    Subtitle,
+    Discount,
+    ButtonWrapper
 } from './cruiseline-super-deals.styles';
+
+import WaterLeft from '../../images/cruiseline/water-left.png';
+import WaterRight from '../../images/cruiseline/water-right.png';
 
 
 const CruiselineSuperDeals = () => (
     <SuperDealsWrapper>
-        <div id='row'>
-            <span>Deal 1</span>
-            <span>Deal 2</span>
-            <span>Deal 3</span>
-        </div>
-        <div id='feature'>Deal 4</div>
+        <Row>
+            <Deal
+                color='white'
+                style={{backgroundImage: `url(${WaterLeft})`}}
+            >
+                <Title>BOOK EARLY</Title>
+                <Subtitle>SAILINGS THROUGH APRIL 2024</Subtitle>
+                <Discount>UP TO $100 ONBOARD CREDIT + MORE</Discount>
+            </Deal>
+            <Deal
+                color='white'
+                style={{backgroundImage: `url(${WaterRight})`}}
+            >
+                <Title>SAIL SOON</Title>
+                <Subtitle>SAILINGS THROUGH DECEMBER 2021</Subtitle>
+                <Discount>UP TO 40% OFF CRUISE RATES</Discount>
+            </Deal>
+        </Row>
+        <Deal featured>
+            <Title>SMILE AND GET AWAY TO HALF MOON CAY</Title>
+            <Subtitle>A PRIVATE ISLAND PARADISE</Subtitle>
+            <ButtonWrapper>LEARN MORE &#9657;</ButtonWrapper>
+        </Deal>
     </SuperDealsWrapper>
 );
 
--- src/components/cruiseline-super-deals/cruiseline-super-deals.styles.js
+++ src/components/cruiseline-super-deals/cruiseline-super-deals.styles.js
@@ -2,27 +2,53 @@ import styled from 'styled-components';
 
 
 export const SuperDealsWrapper = styled.div`
-    border: 1px dotted black;
     width: 100%;
-    div#row {
-        height: 100px;
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0 16px;
-        margin-top: 16px;
+`;
 
-        span {
-            border: 1px solid black;
-            width: 30%;
-            height: 90%;
-        }
-    }
+export const Row = styled.div`
+    height: 185px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 16px 0;
 
-    div#feature {
-        border: 1px solid black;
-        margin: 16px;
-        height: 180px;
+    div + div {
+        margin-left: 8px;
     }
+`;
+
+export const Deal = styled.div`
+    height: ${props => props.featured? `220px`: `inherit`};
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    color: ${props => props.color};
+    background-position: center;
+    background-size: cover;
+
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+export const Title = styled.h2`
+    font-size: 24px;
+    margin: 0 0 12px 0;
+`;
+
+export const Subtitle = styled.h3`
+    font-size: 21px;
+    margin: 0 0 21px 0;
+`;
+
+export const Discount = styled.div`
+    font-size: 21px;
+    font-weight: 700;
+`;
+
+export const ButtonWrapper = styled.button`
+
 `;

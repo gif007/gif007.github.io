151eb61eb39c2c26f1d54cf929ee87da8aca12fe
bard <carruthers.n.t@gmail.com>
2021-07-28 18:05:09 -0300

    filled in about-me and made sizing more responsive

--- src/components/about-me/about-me.component.js
+++ src/components/about-me/about-me.component.js
@@ -9,7 +9,7 @@ import {
 const AboutMe = () => (
     <AboutMeContainer id='about'>
         <HeadingWrapper>About Me</HeadingWrapper>
-        <SummaryWrapper>Hello, I'm a freelance hacker</SummaryWrapper>
+        <SummaryWrapper>A full stack JavaScript developer with front end experience in modern JavaScript, React and Redux. Back end experience in PHP, Python and Node. Innovative, creative, collaborative team player.</SummaryWrapper>
         <LookingForWrapper>I am looking for work</LookingForWrapper>
     </AboutMeContainer>
 );
--- src/components/about-me/about-me.styles.js
+++ src/components/about-me/about-me.styles.js
@@ -3,9 +3,14 @@ import styled from 'styled-components';
 export const AboutMeContainer = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: flex-end;
     width: 65%;
     margin: 0 auto;
     text-align: right;
+
+    @media screen and (max-width: 800px) {
+        width: 90%;
+    }
 `;
 
 export const HeadingWrapper = styled.h1`
@@ -17,6 +22,11 @@ export const SummaryWrapper = styled.span`
     font-size: 1.25rem;
     margin-bottom: 2rem;
     color: rgb(102,102,102);
+    width: 40vw;
+
+    @media screen and (max-width: 800px) {
+        width: 100%;
+    }
 `;
 
 export const LookingForWrapper = styled.span`
